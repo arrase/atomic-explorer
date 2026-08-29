23d125c9850ca8f1abf7a0379f1e5b218fb3479f
Juan Ezquerro LLanes <arrase@gmail.com>
2026-08-30 01:06:38 +0200

    chore: remove desktop application support and simplify for web only

--- vite.config.ts
+++ vite.config.ts
@@ -2,28 +2,11 @@ import { defineConfig } from "vite";
 import wasm from "vite-plugin-wasm";
 import topLevelAwait from "vite-plugin-top-level-await";
 
-const host = process.env.TAURI_DEV_HOST;
-
 export default defineConfig({
   base: "./",
   plugins: [wasm(), topLevelAwait()],
   build: {
     target: "esnext",
   },
-  clearScreen: false,
-  server: {
-    port: 1420,
-    strictPort: true,
-    host: host || false,
-    hmr: host
-      ? {
-          protocol: "ws",
-          host,
-          port: 1421,
-        }
-      : undefined,
-    watch: {
-      ignored: ["**/src-tauri/**"],
-    },
-  },
 });
+
